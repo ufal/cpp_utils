5fa6878525d7d35536eb4abd606b95636fa230ad
Milan Straka <fox@ucw.cz>
2015-11-24 14:55:23 +0100

    Add simple Travis CI test.

---
+++ tests/compile_test.cpp
@@ -0,0 +1,25 @@
+// This file is part of UFAL C++ Utils <http://github.com/ufal/cpp_utils/>.
+//
+// Copyright 2015 Institute of Formal and Applied Linguistics, Faculty of
+// Mathematics and Physics, Charles University in Prague, Czech Republic.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include "binary_decoder.h"
+#include "binary_encoder.h"
+#include "compressor.h"
+#include "iostreams.h"
+#include "iostreams_xml.h"
+#include "new_unique_ptr.h"
+#include "options.h"
+#include "parse_int.h"
+#include "process_args.h"
+#include "split.h"
+#include "string_piece.h"
+#include "threadsafe_stack.h"
+
+int main(void) {
+  return 0;
+}
