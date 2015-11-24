f3c3becece603cbea561cd9884ed4568161f8fe2
Milan Straka <fox@ucw.cz>
2015-11-24 20:35:53 +0100

    Use all inline functions in compile_test.

--- tests/compile_test.cpp
+++ tests/compile_test.cpp
@@ -21,6 +21,86 @@
 #include "string_piece.h"
 #include "threadsafe_stack.h"
 
+using namespace ufal::utils;
+
+void binary_decoder_compilation() {
+  binary_decoder dec;
+  string str;
+
+  dec.fill(10);
+  dec.next_1B();
+  dec.next_2B();
+  dec.next_4B();
+  dec.next_str(str);
+  dec.next<char>(1);
+
+  dec.is_end();
+  dec.seek(dec.tell());
+}
+
+void binary_encoder_compilation() {
+  binary_encoder enc;
+  enc.add_1B(1);
+  enc.add_2B(2);
+  enc.add_4B(4);
+  enc.add_float(1.);
+  enc.add_double(1.);
+  enc.add_str("a");
+  enc.add_data("a");
+  enc.add_data(vector<int>{1,2,3});
+  enc.add_data("a", 1);
+}
+
+void iostreams_compilation() {
+  string str;
+
+  iostreams_init();
+  iostreams_init_binary_input();
+  iostreams_init_binary_output();
+  getpara(cin, str);
+}
+
+void iostreams_xml_compilation() {
+  string str;
+
+  cout << xml_encoded("a") << xml_encoded(str, true);
+}
+
+void new_unique_ptr_compilation() {
+  auto ptr = new_unique_ptr<int>(1);
+}
+
+void options_compilation(int argc, char* argv[]) {
+  options::map m;
+  options::parse({{"none", options::value::none},{"any", options::value::any}, {"set", options::value{"set"}}}, argc, argv, m);
+}
+
+void parse_double_compilation() {
+  parse_double("1.23e-45", "number");
+}
+
+void parse_int_compilation() {
+  parse_int("1", "number");
+}
+
+void process_args_compilation(int argi, int argc, char* argv[]) {
+  process_args(argi, argc, argv, [](istream&, ostream&, string_piece){}, "a");
+}
+
+void split_compilation() {
+  vector<string> strings;
+  vector<string_piece> pieces;
+
+  split("1 2 3", ' ', strings);
+  split("1 2 3", ' ', pieces);
+}
+
+void threadsafe_stack_compilation() {
+  threadsafe_stack<int> stack;
+  stack.push(nullptr);
+  stack.pop();
+}
+
 int main(void) {
   return 0;
 }
