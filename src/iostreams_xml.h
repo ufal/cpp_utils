1f17a84c19319f05a57b0af6c179aafe17e6aa5c
Milan Straka <fox@ucw.cz>
2015-11-24 20:35:37 +0100

    Use string_piece in xml_encoded.

--- src/iostreams_xml.h
+++ src/iostreams_xml.h
@@ -21,13 +21,11 @@ namespace utils {
 // Print xml content while encoding <>& and optionally " using XML entities.
 class xml_encoded {
  public:
-  xml_encoded(const string& str, bool encode_quot = false) : text(str.c_str()), length(str.size()), encode_quot(encode_quot) {}
-  xml_encoded(const char* str, size_t length, bool encode_quot = false) : text(str), length(length), encode_quot(encode_quot) {}
+  xml_encoded(string_piece str, bool encode_quot = false) : str(str), encode_quot(encode_quot) {}
 
   friend ostream& operator<<(ostream& os, xml_encoded data);
  private:
-  const char* text;
-  size_t length;
+  string_piece str;
   bool encode_quot;
 };
 
@@ -38,21 +36,22 @@ inline ostream& operator<<(ostream& os, xml_encoded data);
 //
 
 ostream& operator<<(ostream& os, xml_encoded data) {
-  const char* to_print = data.text;
+  string_piece& str = data.str;
+  const char* to_print = str.str;
 
-  while (data.length) {
-    while (data.length && *data.text != '<' && *data.text != '>' && *data.text != '&' && (!data.encode_quot || *data.text != '"'))
-      data.text++, data.length--;
+  while (str.len) {
+    while (str.len && *str.str != '<' && *str.str != '>' && *str.str != '&' && (!data.encode_quot || *str.str != '"'))
+      str.str++, str.len--;
 
-    if (data.length) {
-      if (to_print < data.text) os.write(to_print, data.text - to_print);
-      os << (*data.text == '<' ? "&lt;" : *data.text == '>' ? "&gt;" : *data.text == '&' ? "&amp;" : "&quot;");
-      data.text++, data.length--;
-      to_print = data.text;
+    if (str.len) {
+      if (to_print < str.str) os.write(to_print, str.str - to_print);
+      os << (*str.str == '<' ? "&lt;" : *str.str == '>' ? "&gt;" : *str.str == '&' ? "&amp;" : "&quot;");
+      str.str++, str.len--;
+      to_print = str.str;
     }
   }
 
-  if (to_print < data.text) os.write(to_print, data.text - to_print);
+  if (to_print < str.str) os.write(to_print, str.str - to_print);
 
   return os;
 }
