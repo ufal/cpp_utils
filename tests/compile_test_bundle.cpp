f5d26f88bb41881d188b7167a110fccc9c70f7b8
Milan Straka <milan@strakovi.com>
2016-03-03 10:29:33 +0100

    Add compile_test_bundle compiling all sources in one compilation unit.

---
+++ tests/compile_test_bundle.cpp
@@ -0,0 +1,14 @@
+// This file is part of UFAL C++ Utils <http://github.com/ufal/cpp_utils/>.
+//
+// Copyright 2015 Institute of Formal and Applied Linguistics, Faculty of
+// Mathematics and Physics, Charles University in Prague, Czech Republic.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include "compile_test.cpp"
+#include "compressor_load.cpp"
+#include "compressor_save.cpp"
+#include "options.cpp"
+#include "url_detector.cpp"
